9ce42ced617fd8fda3e309c4003dac3b33480a37
Student <daotinh0410@gmail.com>
2022-02-14 10:38:48 +0700

    1_a

---
+++ app/src/main/java/com/example/a18035741_daohuuminhtinh_lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a18035741_daohuuminhtinh_lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
